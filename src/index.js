085ef2ebf91b62c009d37d7f5045221cad8e39d5
JakhongirMusayev <DevDar76uzb@gmail.com>
2021-08-11 17:10:57 +0500

    html & http-server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Qiziqarli");
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!'
+
+// добавляем заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'none',
@@ -9,4 +10,11 @@ module.exports = {
 		path: path.resolve(__dirname, './dist'),
 		filename: '[name].bundle.js',
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'webpack Boilerplate',
+			template: path.resolve(__dirname, './src/template.html'), // шаблон
+			filename: 'index.html', // название выходного файла
+		}),
+	],
 }
